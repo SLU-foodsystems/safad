541892226eab8d51d9dae17bf3f3fd2fac4d9b69
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-01-30 09:42:59 +0100

    EAT and FBS sums now are kept up to date

--- src/components/FoodsCard.ts
+++ src/components/FoodsCard.ts
@@ -50,43 +50,53 @@ function observeAttributeChanges(
   observer.observe(rootEl, config);
 }
 
-function FbsBlock(fbs: FBS, mode: "sum" | "mean") {
+function FbsBlock(fbs: FBS, mode: "sum" | "mean", notifyChange) {
   const root = el("div", "foods-card__fbs");
 
   const header = el("div", "cluster cluster--between");
   const title = el("h4");
   title.innerText = fbs.name;
-  const fbsValueLabel = el("span");
-  title.appendChild(fbsValueLabel);
+
+  const inputHeader = el("span", "u-faded");
+  const aggregateSpan = el("span");
+  aggregateSpan.innerHTML = "0"; // Default value.
+  const unitLabel = el("span", "foods-card__unit");
+  unitLabel.innerText = " g / day"; // TODO
+  inputHeader.appendChild(aggregateSpan);
+  inputHeader.appendChild(unitLabel);
+
   header.appendChild(title);
+  header.appendChild(inputHeader);
 
-  const unitLabel = el("span", "foods-card__unit u-faded");
-  unitLabel.innerText = "g / day"; // TODO
+  const inputs: { [k: string]: HTMLInputElement } = {};
 
-  const onUpdate = () => {
-    const values = Object.values(inputs)
-      .map((x) => x.value)
-      .map((val) => {
-        if (!val) return 0;
-        const parseable = val.trim().replace(",", ".");
+  const getInputValues = (): number[] =>
+    Object.values(inputs)
+      .map((input) => input.value)
+      .map((value) => {
+        if (!value) return 0;
+        const parseable = value.trim().replace(",", ".");
         // TODO: Error handling.
         const number = Number.parseFloat(parseable);
         return Number.isNaN(number) ? 0 : number;
       });
 
+  // TODO: Do two updates here: one for self, one for eat
+  // alt: notifyChange()
+  const onSuaUpdate = () => {
+    const values = getInputValues();
     const value = mode === "sum" ? sum(values) : average(values);
-    fbsValueLabel.innerText = String(toPrecision(value));
+    aggregateSpan.innerText = String(toPrecision(value));
   };
-
-  const inputs: Record<string, HTMLInputElement> = {};
 
   const suaRows = fbs.sua.map(({ name, id }) => {
     const row = el("div", "foods-card__sua cluster cluster--between");
     const span = el("span");
     span.innerText = name;
 
-    const input = el("input", "", { type: "text" });
-    input.addEventListener("change", onUpdate);
+    const input = el("input", "", { type: "text", "data-sua-id": id });
+    input.addEventListener("input", onSuaUpdate);
+    input.addEventListener("change", notifyChange);
 
     row.appendChild(span);
     row.appendChild(input);
@@ -119,19 +129,44 @@ export default function FoodsCard(eat: EAT) {
   };
 
   headerButton.innerHTML = `
+    <span>${eat.name}</span>
     <span>
-      ${eat.name}: <span data-js-amount>103</span> g
+      <span data-fbs-aggregate>0</span> g
+      <svg viewBox="0 0 10 10" aria-hidden="true" focusable="false">
+        <rect class="vert" height="8" width="2" y="1" x="4" />
+        <rect height="2" width="8" y="4" x="1" />
+      </svg>
     </span>
-    <svg viewBox="0 0 10 10" aria-hidden="true" focusable="false">
-      <rect class="vert" height="8" width="2" y="1" x="4" />
-      <rect height="2" width="8" y="4" x="1" />
-    </svg>
   `;
+
+  const aggregateSpan = headerButton.querySelector('[data-fbs-aggregate]') as HTMLSpanElement | null;
+  if (!aggregateSpan) throw new Error("AggregateSpan not found.")
 
   const body = el("div", "foods-card__body", {
     "data-accordion-body": "true",
   });
-  eat.fbs.forEach((fbs) => body.appendChild(FbsBlock(fbs, "sum")));
+
+  // TODO: Assumes SUM and not MEAN
+  const updateFbsSum = () => {
+    const inputs = body.querySelectorAll(
+      "input[data-sua-id]"
+    ) as NodeListOf<HTMLInputElement>;
+    if (!inputs) throw new Error("Could not find sua inputs");
+
+    const values = Array.from(inputs)
+      .map((el) => el.value)
+      .map((val) => parseFloat(val))
+      .filter((val) => !Number.isNaN(val));
+
+    const value = sum(values);
+
+    aggregateSpan.innerText = String(toPrecision(value, 2));
+  };
+
+
+  eat.fbs.forEach((fbs) =>
+    body.appendChild(FbsBlock(fbs, "sum", updateFbsSum))
+  );
 
   root.appendChild(header);
   root.appendChild(body);
