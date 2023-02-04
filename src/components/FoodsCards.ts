79590b465479bc71bc3f64793f2662f394666e98
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-02-04 17:02:34 +0100

    Initiate move to vue.js - add boilerplate

--- src/components/FoodsCards.ts
+++
@@ -1,5 +0,0 @@
-import FoodsCard from "./FoodsCard";
-
-export default function FoodsCards(data: EAT[]) {
-  return data.map(FoodsCard);
-}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-import "./styles/main.scss";
-import foodCodes from "./foods.json";
-import FoodsCards from "./components/FoodsCards";
-import setupFoodsCardsMenuButtons from "./lib/setup-foods-cards-menu-buttons";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const cards = FoodsCards(foodCodes.data);
+import './assets/main.css'
 
-const parent = document.querySelector(".diet-configuration");
-cards.forEach(card => parent?.appendChild(card));
-
-setupFoodsCardsMenuButtons("#js-expand-all", "#js-collapse-all");
+createApp(App).mount('#app')
