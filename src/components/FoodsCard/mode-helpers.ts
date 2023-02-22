582e440d7855a32d4fb19b996a67054075c53d74
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-02-22 15:59:58 +0100

    Update mode-helpers
    Honestly not sure if I will keep this file at all

--- src/components/FoodsCard/mode-helpers.ts
+++ src/components/FoodsCard/mode-helpers.ts
@@ -1,22 +1,25 @@
 import { average, sum, toPrecision } from "@/lib/utils";
-export type Mode = "amount" | "percentage";
+export type Mode = "amount" | "factors" | "origin";
 
 export function isMode(value: any): value is Mode {
   switch (value) {
     case "amount":
-    case "percentage":
+    case "factors":
+    case "origin":
       return true;
     default:
       return false;
   }
 }
 
-export function unit(mode: Mode, short = false) {
+export function unit(mode: Mode, short = false): string {
   switch (mode) {
     case "amount":
       return short ? "g" : "g/day";
-    case "percentage":
+    case "factors":
       return "%";
+    case "origin":
+      return "";
   }
 }
 
@@ -24,7 +27,8 @@ export function limits(mode: Mode): [number, number] {
   switch (mode) {
     case "amount":
       return [0, Number.POSITIVE_INFINITY];
-    case "percentage":
+    case "origin":
+    case "factors":
       return [0, 100];
   }
 }
@@ -33,8 +37,10 @@ export function aggregate(values: number[], mode: Mode): number {
   switch (mode) {
     case "amount":
       return toPrecision(sum(values));
-    case "percentage":
+    case "factors":
       return toPrecision(average(values));
+    case "origin":
+      return 0;
   }
 }
 
