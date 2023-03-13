8d2d240a38858f682bcdc6c3bd6049c01eeb8d7b
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-03-13 21:09:58 +0100

    Fixup reference to publicDir for vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
+  publicDir: "../public",
   build: {
     outDir: "../dist",
   },
