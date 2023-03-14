690d7f368036034715ea4f273e7b21a8874b0e34
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-03-14 18:28:56 +0100

    Abstract splitCsvString to misc/utils.mjs

--- misc/foods-csv-to-json.mjs
+++ misc/foods-csv-to-json.mjs
@@ -21,48 +21,7 @@
  * Expects the path to the csv file as parameter.
  */
 import fs from "fs";
-
-const CSV_DELIM = ",";
-
-/**
- * Split a string into a vector of parts by the CSV_DELIM, while ignoring any
- * occurances inside double-quotes.
- */
-function splitCsvRow(str) {
-  if (!str.includes('"')) return str.split(CSV_DELIM);
-
-  const splitPoints = [];
-  let posDelim = str.indexOf(CSV_DELIM, -1);
-  let posQuote = str.indexOf('"', -1);
-
-  // Repeat until we've iterated over all quotes
-  while (posQuote > -1) {
-    // Add all commas up to the quote to the splitPoints.
-    while (posDelim < posQuote) {
-      splitPoints.push(posDelim);
-      posDelim = str.indexOf(CSV_DELIM, posDelim + 1);
-    }
-
-    let posQuoteEnd = str.indexOf('"', posQuote + 1);
-    posQuote = str.indexOf('"', posQuoteEnd + 1);
-    posDelim = str.indexOf(CSV_DELIM, posQuoteEnd);
-  }
-
-  while (posDelim > -1) {
-    splitPoints.push(posDelim);
-    posDelim = str.indexOf(CSV_DELIM, posDelim + 1);
-  }
-
-  // Add the last index, just to make the iteration below easier
-  splitPoints.push(str.length - 1);
-
-  let prev = 0;
-  return splitPoints.map((idx) => {
-    const part = str.substring(prev, idx);
-    prev = idx + 1;
-    return part;
-  });
-}
+import { splitCsvRow } from "./utils.mjs";
 
 function main(args) {
   const csvPath = args[2];
@@ -70,7 +29,7 @@ function main(args) {
   const lines = fileContent.split("\n");
   lines.pop();
 
-  const matrix = lines.map(splitCsvRow);
+  const matrix = lines.map(row => splitCsvRow(row, CSV_DELIM));
 
   const result = { data: [] };
 
---
+++ misc/utils.mjs
@@ -0,0 +1,49 @@
+/**
+ * Split a string into a vector of parts by the CSV_DELIM, while ignoring any
+ * occurances inside double-quotes.
+*
+* NOTE: this assumes 
+ */
+export function splitCsvRow(str, delimitor) {
+  if (!str.includes('"')) return str.split(delimitor);
+  let sanityCounter = 0;
+
+  const splitPoints = [];
+  // Position of the delimitor (normally a comma)
+  let posDelim = str.indexOf(delimitor, -1);
+  // Position of the quote
+  let posQuote = str.indexOf('"', -1);
+
+  // Repeat until we've iterated over all quotes
+  while (posQuote > -1) {
+    if (sanityCounter++ > 2000) {
+      throw new Error("The script seems to have got stuck in an endless loop. Input was:\n\t" + str)
+    }
+    // Add all commas up to the quote to the splitPoints.
+    while (posDelim < posQuote) {
+      splitPoints.push(posDelim);
+      posDelim = str.indexOf(delimitor, posDelim + 1);
+    }
+
+    let posQuoteEnd = str.indexOf('"', posQuote + 1);
+    posQuote = str.indexOf('"', posQuoteEnd + 1);
+    posDelim = str.indexOf(delimitor, posQuoteEnd);
+
+  }
+
+  while (posDelim > -1) {
+    console.log("Outer loop 2");
+    splitPoints.push(posDelim);
+    posDelim = str.indexOf(delimitor, posDelim + 1);
+  }
+
+  // Add the last index, just to make the iteration below easier
+  splitPoints.push(str.length - 1);
+
+  let prev = 0;
+  return splitPoints.map((idx) => {
+    const part = str.substring(prev, idx);
+    prev = idx + 1;
+    return part;
+  });
+}
