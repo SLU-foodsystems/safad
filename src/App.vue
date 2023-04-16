fdfb46f426fc402ddcd24761af9a5087a4047766
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-04-16 13:32:18 +0200

    Test a diet reduction

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import reduceDiet from "./lib/rpc-reducer";
+import foodsRecipes from "./data/foodex-recipes.json";
 
 export default defineComponent({
   data() {
     return {};
   },
 
-  methods: {},
+  methods: {
+    run() {
+      const diet = [
+        ["A.01.06.001.004", 2000, 10, 10, 33],
+        ["A.01.07.001.006", 1000, 50, 20, 0],
+        ["I.14.07.001.002", 500, 0, 30, 0],
+      ].map((x) => ({
+        code: x[0] as string,
+        amount: x[1] as number,
+        organic: x[2] as number,
+        consumerWaste: x[3] as number,
+        retailWaste: x[4] as number,
+      }));
+      const recipes = foodsRecipes.data as unknown;
+
+      type FoodsRecipe = [string, string, number, number][];
+      type FoodsRecipes = { [foodexCode: string]: FoodsRecipe };
+      reduceDiet(diet, recipes as FoodsRecipes);
+    },
+  },
 });
 </script>
 
@@ -18,7 +39,7 @@ export default defineComponent({
       </div>
       <h2>SLU Foods Benchmarker</h2>
       <div class="cluster cluster--center">
-        <button class="button button--accent">Run &gt;</button>
+        <button class="button button--accent" @click="run">Run &gt;</button>
       </div>
     </div>
   </section>
@@ -40,7 +61,7 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
 
-  > div {
+  >div {
     flex-basis: 30em;
   }
 
