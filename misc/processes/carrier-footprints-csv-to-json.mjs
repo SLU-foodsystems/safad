629853607c9f9c814c7e5cf4d810d3ca1dd6efce
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-05-23 15:27:00 +0200

    Add scripts and data for carrier-footprints

---
+++ misc/processes/carrier-footprints-csv-to-json.mjs
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+/**
+ * Converts a csv with footprints of energy types to a json file.
+ * Note that Electricty is the only carrier which has country-specific
+ * parameters
+ *
+ * Input:
+ * Energy carrier,Country,CountryCode,Co2,ch4,n2o
+ *
+ * Output:
+ * {
+ *   data: {
+ *     Electricty: { [country]: [co2, ch4, n2o]}
+ *     [carrier]: [co2, ch4, n2o],
+ *   }
+ * } as JSON
+ *
+ * Usage:
+ *
+ *  node carrier-footsprints-csv-to-json.mjs "./carrier-footprints.csv" >
+ *    ../../src/data/carrier-footprints.json
+ */
+
+import { readCsv } from "../utils.mjs";
+
+function main(args) {
+  const csv = readCsv(args[0], ",", true).slice(1); // Drop Header
+
+  const results = {
+    Electricity: {},
+  };
+
+  csv.forEach(([carrier, country, _countryCode, ...footprintsStr]) => {
+    const footprints = footprintsStr.map((x) => (x ? parseFloat(x) : 0));
+    if (carrier === "Electricity") {
+      results[carrier][country] = footprints;
+    } else {
+      results[carrier] = footprints;
+    }
+  });
+
+  console.log(JSON.stringify({ data: results }, null, 0));
+}
+
+main(process.argv.slice(2));
