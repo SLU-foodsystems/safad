dd92bd24cd61d4e51bd90f51f80f259594dfb250
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-06-12 15:36:01 +0200

    WIP in chart file

--- src/components/ChartContainer.vue
+++ src/components/ChartContainer.vue
@@ -4,83 +4,70 @@ import BoundariesChart from "../lib/charts/BoundariesChart";
 import StackedBarChart from "@/lib/charts/StackedBarChart";
 import downloadSvgAsImage from "@/lib/charts/d3-exporter";
 
+import exampleDietJson from "@/data/example-impacts.json";
+import categoryNamesJson from "@/data/category-names.json";
+import { mapValues, vectorSum } from "@/lib/utils";
+
+const exampleDiet = exampleDietJson as Record<string, number[]>;
+const categoryNames = categoryNamesJson as Record<string, string>;
+
 export default defineComponent({
   mounted() {
-    const boundariesData = [
-      [
-        { axis: "GHG Emissions", value: 1.22 },
-        { axis: "Cropland Use", value: 2.28 },
-        { axis: "N Application", value: 2.29 },
-        { axis: "P Application", value: 0.77 },
-        { axis: "Water Use", value: 2.22 },
-        { axis: "Extinction Rate", value: 0.92 },
-      ],
-    ];
+    // const boundariesData = [
+    //   [
+    //     { axis: "GHG Emissions", value: 1.22 },
+    //     { axis: "Cropland Use", value: 2.28 },
+    //     { axis: "N Application", value: 2.29 },
+    //     { axis: "P Application", value: 0.77 },
+    //     { axis: "Water Use", value: 2.22 },
+    //     { axis: "Extinction Rate", value: 0.92 },
+    //   ],
+    // ];
 
-    BoundariesChart(".boundaries-chart-container", boundariesData, {
-      maxValue: 1,
+    // BoundariesChart(".boundaries-chart-container", boundariesData, {
+    //   maxValue: 2,
+    // });
+
+    const labels = [
+      "GHG Emissions",
+      "Cropland Use",
+      "N Application",
+      "P Application",
+      "Water Use",
+      "Extinction Rate",
+    ];
+    const indices = [5, 16, 17, 18, 19, 20];
+    const diet = mapValues(exampleDiet, (numbers) =>
+      indices.map((i) => numbers[i])
+    );
+
+    const resultsPerCategory: Record<string, number[]> = {};
+    Object.keys(diet).forEach((rpcCode) => {
+      const categoryCode = rpcCode.substring(0, 4);
+      if (!resultsPerCategory[categoryCode]) {
+        resultsPerCategory[categoryCode] = diet[rpcCode];
+      } else {
+        resultsPerCategory[categoryCode] = vectorSum(
+          resultsPerCategory[categoryCode],
+          diet[rpcCode]
+        );
+      }
     });
 
-    const data = [
-      {
-        category: "GHG Emissions",
-        diary: 0.2,
-        meatPoultry: 0.3,
-        fruitsVeg: 0.1,
-        fishSeafood: 0.15,
-        cereals: 0.15,
-        other: 0.1,
-      },
-      {
-        category: "Cropland Use",
-        diary: 0.1,
-        meatPoultry: 0.35,
-        fruitsVeg: 0.1,
-        fishSeafood: 0,
-        cereals: 0.35,
-        other: 0.1,
-      },
-      {
-        category: "N Application",
-        diary: 0.1,
-        meatPoultry: 0.35,
-        fruitsVeg: 0.15,
-        fishSeafood: 0.05,
-        cereals: 0.35,
-        other: 0.0,
-      },
-      {
-        category: "P Application",
-        diary: 0.25,
-        meatPoultry: 0.1,
-        fruitsVeg: 0.5,
-        fishSeafood: 0.05,
-        cereals: 0.05,
-        other: 0.05,
-      },
-      {
-        category: "Water Use",
-        diary: 0.1,
-        meatPoultry: 0.2,
-        fruitsVeg: 0.35,
-        fishSeafood: 0.1,
-        cereals: 0.1,
-        other: 0.15,
-      },
-      {
-        category: "Extinction Rate",
-        diary: 0.3,
-        meatPoultry: 0.2,
-        fruitsVeg: 0.1,
-        fishSeafood: 0.15,
-        cereals: 0.15,
-        other: 0.1,
-      },
-    ].map((o) =>
-      Object.fromEntries(
-        Object.entries(o).map(([k, v]) => [k, k === "category" ? v : String(v)])
-      )
+    const categories = Object.keys(resultsPerCategory).filter(
+      (x) => x[0] === "A"
     );
+
+    const data = labels.map((category, i) => {
+      const datum: Record<string, string|number> = { category };
+      categories.forEach((code) => {
+        datum[code] = String(resultsPerCategory[code][i]);
+      });
+
+      return datum;
+    });
+
+    console.log(data)
 
     const columns = Object.keys(data[0]).filter((x) => x !== "category");
     StackedBarChart(".bar-chart-container", data, columns, {});
