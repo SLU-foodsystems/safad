7b268ca5b5f7f5364942d6370830c3e526c418f9
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-06-13 12:22:41 +0200

    Add comments to preprocessing script

--- misc/preprocessing.sh
+++ misc/preprocessing.sh
@@ -2,23 +2,35 @@
 
 set -eu
 
+# Carrier footprints: GHG per energy type for each country
 node ./carrier-footprints/carrier-footprints-csv-to-json.mjs ./carrier-footprints/carrier-footprints.csv \
   > ../src/data/carrier-footprints.json
 
+# Processes energy demands: Energy req. of each process
 node ./processes-energy-demand/processes-energy-demand-csv-json.mjs ./processes-energy-demand/processes-energy-demand.csv > ../src/data/processes-energy-demands.json
 
+
+# Flat environmental factors - without import factors, mostly for verification
 node ./env-factors/flat-env-factors-csv-to-json.mjs ./env-factors/env-factors.csv > ../src/data/env-factors-flat.json
 
+# Environmental factors with import factors
 node ./env-factors/env-factors-csv-to-json.mjs ./env-factors/env-factors.csv > ../src/data/env-factors.json
 
+# RPC parameters: done in two steps to allow for, in the future, to manually
+# adjust the values if we want to.
+# Step a: Generate csv files
 node ./rpc-parameters/generate-csv-per-country.mjs France Germany Greece Hungary Ireland Italy Poland Spain Sweden
 
+# Step b: Convert csv to json
 for country in France Germany Greece Hungary Ireland Italy Poland Spain Sweden; do
   node ./rpc-parameters/rpc-parameters-csv-to-json.mjs "./rpc-parameters/csv-out/$country-rpc.csv" > "../src/data/rpc-parameters/$country-rpc.json"
 done
 
+# Extract all category names
 node ./foodex-names/extract-category-names.mjs ./foodex-names/foodex-names.csv 2 > ../src/data/category-names.json
 
+# Create a json for mapping rpc codes to sua names
 node ./rpc-sua-map/rpc-sua-csv-to-json.mjs ./rpc-sua-map/rpc-to-sua.csv > ../src/data/rpc-to-sua.json
 
+# Create a json representation of the csvs
 node ./foodex-recipes/recipes-csv-parser.mjs ./foodex-recipes/foodex-recipes.csv ./foodex-recipes/foods-and-processes.csv > ../src/data/foodex-recipes.json
---
+++ misc/waste-factors/waste-factors.mjs
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+/**
+ * Parse a csv with retail and consumer wastes into a JSON object.
+ *
+ * Takes on argument, and that is the path to the csv file.
+ */
+
+
+import { readCsv } from "../utils.mjs";
+
+function main(args) {
+  const wasteCsv = readCsv(args[0], ",");
+
+  /** @type Object.<string, Object.<string, [number, number]>> */
+  const results = {};
+  wasteCsv.forEach(([foodCode, _foodName, countryName, _countryCode, retailWaste, consumerWaste]) => {
+    if (!results[countryName]) results[countryName] = {};
+    const wastes = [retailWaste, consumerWaste].map(x => {
+      const parsed = parseFloat(x)
+      const number = Number.isNaN(parsed) ? 0 : parsed;
+      return Math.max(0, number); // Ensure non-negative wastes
+    });
+    results[countryName][foodCode] = wastes;
+  });
+
+  console.log(JSON.stringify(results, null, 2));
+}
+
+main(process.argv.slice(2));
