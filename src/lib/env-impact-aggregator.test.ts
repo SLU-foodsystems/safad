2586488199357cdacc5ac685777541fe4c0d5ac7
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-06-13 14:47:09 +0200

    Update tests for env-impact-aggregator

--- src/lib/env-impact-aggregator.test.ts
+++ src/lib/env-impact-aggregator.test.ts
@@ -1,31 +1,35 @@
 import { describe, test, expect } from "vitest";
 import joinEnvImpactSheet from "./env-impact-aggregator";
+import { N_ENV_FOOTPRINTS } from "./constants";
+
+const constEnvFactors = (x: number) =>
+  Array.from({ length: N_ENV_FOOTPRINTS }).map((_) => x);
 
 describe("env-impacts.ts", () => {
   test("basic functionality", () => {
     const rpcFactors: RpcFactors = {
       a: {
-        se: [0.20, 0.10, 0],
-        es: [0.50, 0.15, 0],
-        RoW: [0.30, 0.20, 0],
+        se: [0.2, 0.1, 0],
+        es: [0.5, 0.15, 0],
+        RoW: [0.3, 0.2, 0],
       },
       b: {
-        en: [0.50, 0, 0],
-        fr: [0.40, 0.15, 0],
-        RoW: [0.10, 0.20, 0],
+        en: [0.5, 0, 0],
+        fr: [0.4, 0.15, 0],
+        RoW: [0.1, 0.2, 0],
       },
     };
 
     const envImpactSheet = {
       a: {
-        se: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] as EnvFactors,
-        es: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3] as EnvFactors,
-        RoW: [8, 8, 8, 8, 8, 8, 8, 8, 8, 8] as EnvFactors,
+        se: constEnvFactors(1) as EnvFactors,
+        es: constEnvFactors(3) as EnvFactors,
+        RoW: constEnvFactors(8) as EnvFactors,
       },
       b: {
-        en: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] as EnvFactors,
-        fr: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4] as EnvFactors,
-        RoW: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10] as EnvFactors,
+        en: constEnvFactors(1) as EnvFactors,
+        fr: constEnvFactors(4) as EnvFactors,
+        RoW: constEnvFactors(10) as EnvFactors,
       },
     };
 
@@ -37,15 +41,15 @@ describe("env-impacts.ts", () => {
     expect(result).toHaveProperty("a");
     expect(result).toHaveProperty("b");
 
-    expect(result.a).toHaveLength(10);
-    expect(result.b).toHaveLength(10);
+    expect(result.a).toHaveLength(N_ENV_FOOTPRINTS);
+    expect(result.b).toHaveLength(N_ENV_FOOTPRINTS);
 
-    const expectedAs = Array.from({ length: 10 }).map(
-      (_) => (0.2 * 1) / 0.9 + (0.5 * 3) / 0.85 + (0.3 * 8) / 0.8
+    const expectedAs = constEnvFactors(
+      (0.2 * 1) / 0.9 + (0.5 * 3) / 0.85 + (0.3 * 8) / 0.8
     );
 
-    const expectedBs = Array.from({ length: 10 }).map(
-      (_) => (0.5 * 1) / 1.0 + (0.4 * 4) / 0.85 + (0.1 * 10) / 0.8
+    const expectedBs = constEnvFactors(
+      (0.5 * 1) / 1.0 + (0.4 * 4) / 0.85 + (0.1 * 10) / 0.8
     );
 
     expect(result.a).toEqual(expectedAs);
@@ -55,24 +59,23 @@ describe("env-impacts.ts", () => {
   describe("organic and conventional split", () => {
     const rpcFactors = (shareOrganic: number): RpcFactors => ({
       a: {
-        se: [0.20, 0.10, shareOrganic],
-        es: [0.50, 0.15, shareOrganic],
-        RoW: [0.30, 0.20, shareOrganic],
+        se: [0.2, 0.1, shareOrganic],
+        es: [0.5, 0.15, shareOrganic],
+        RoW: [0.3, 0.2, shareOrganic],
       },
     });
 
     const envImpactSheet = {
       a: {
-        se: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] as EnvFactors,
-        es: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2] as EnvFactors,
-        RoW: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3] as EnvFactors,
+        se: constEnvFactors(1) as EnvFactors,
+        es: constEnvFactors(2) as EnvFactors,
+        RoW: constEnvFactors(3) as EnvFactors,
       },
     };
 
     const getExpected = (ratio: number) =>
-      Array.from({ length: 10 }).map(
-        (_) =>
-          (0.2 * 1 * ratio) / (1 - 0.1) +
+      constEnvFactors(
+        (0.2 * 1 * ratio) / (1 - 0.1) +
           (0.5 * 2 * ratio) / (1 - 0.15) +
           (0.3 * 3 * ratio) / (1 - 0.2)
       );
@@ -112,12 +115,12 @@ describe("env-impacts.ts", () => {
     test("Even split", () => {
       const resultC = joinEnvImpactSheet(
         envImpactSheet,
-        rpcFactors(0.50),
+        rpcFactors(0.5),
         "conventional"
       );
       const resultO = joinEnvImpactSheet(
         envImpactSheet,
-        rpcFactors(0.50),
+        rpcFactors(0.5),
         "organic"
       );
       expect(resultC).toHaveProperty("a");
@@ -131,7 +134,7 @@ describe("env-impacts.ts", () => {
     });
 
     test("Uneven split", () => {
-      const _rpcFactors = rpcFactors(0.30);
+      const _rpcFactors = rpcFactors(0.3);
       const resultC = joinEnvImpactSheet(
         envImpactSheet,
         _rpcFactors,
