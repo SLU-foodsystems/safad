b3bd1a54a1d3850a01e030839a45a0d52190155f
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-06-19 13:29:48 +0200

    Bugfix: N and P are already in kg

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,8 +7,8 @@ export const ENV_IMPACTS_ZERO = Array.from({
 export const PLANETARY_BOUNDARY_LIMITS = {
   co2e: (0.67 * 1000) / 365, // from tonnes per year to g per day
   land: (0.174 * 10000) / 365, // from ha per year to m2 per day
-  n: 12 / (1000 * 365), // kg per year -> g per day
-  p: 1.1 / (1000 * 365), // kg per year -> g per day
+  n: 12 / 365, // kg per year -> kg per day
+  p: 1.1 / 365, // kg per year -> kg per day
   h2o: 334 / 365, // m3, per year -> per day
   biodiversity: 1.33e-9 / 365, // unitless? -> per day
 };
