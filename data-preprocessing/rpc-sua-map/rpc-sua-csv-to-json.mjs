62888a3b0eb06fcf7c96c2d543e3647f9dd3a960
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-10-30 14:33:16 +0100

    Simplify code for rpc-sua-csv map

--- data-preprocessing/rpc-sua-map/rpc-sua-csv-to-json.mjs
+++ data-preprocessing/rpc-sua-map/rpc-sua-csv-to-json.mjs
@@ -8,16 +8,11 @@ import { readCsv } from "../utils.mjs";
 function main(args) {
   const csv = readCsv(args[0], ",").slice(1); // Slice to drop header
 
-  const map = {};
-  const handledFoodExCodes = new Set();
-
-  csv.forEach(
-    ([_foodCode, foodExCode, _foodExName, suaCode, _suaName]) => {
-      if (handledFoodExCodes.has(foodExCode)) return;
-
-      handledFoodExCodes.add(foodExCode);
-      map[foodExCode] = (suaCode || "").trim();
-    }
+  const map = Object.fromEntries(
+    csv.map(([_foodCode, foodExCode, _foodExName, suaCode, _suaName]) => [
+      foodExCode.trim(),
+      suaCode.trim() || "",
+    ])
   );
 
   console.log(JSON.stringify(map, null, 0));
