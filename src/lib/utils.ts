c1415e981cfa32801d2859d12e518d65d52dcf72
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-11-01 18:32:06 +0100

    Add csv parsing function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -149,3 +149,42 @@ export const nthIndexOf = (
 
   return nthIndexOf(string, searchString, nth - 1, index + 1);
 };
+
+/**
+ * Split a csv-document into a 2d-array, while ignoring any delimiter-
+ * occurances inside double-quotes.
+ *
+ * e.g. 'hello, world, 3, "foo, bar", 10' ->
+ *      [['hello', 'world', 3, 'foo, bar', 10]]
+ */
+export function parseCsvFile(fileContent: string, delim = ",") {
+  let line = [""];
+  const ret = [line];
+  let quote = false;
+
+  for (let i = 0; i < fileContent.length; i++) {
+    const cur = fileContent[i];
+    const next = fileContent[i + 1];
+
+    if (!quote) {
+      const cellIsEmpty = line[line.length - 1].length === 0;
+      if (cur === '"' && cellIsEmpty) quote = true;
+      else if (cur === delim) line.push("");
+      else if (cur === "\r" && next === "\n") {
+        line = [""];
+        ret.push(line);
+        i++;
+      } else if (cur === "\n" || cur === "\r") {
+        line = [""];
+        ret.push(line);
+      } else line[line.length - 1] += cur;
+    } else {
+      if (cur === '"' && next === '"') {
+        line[line.length - 1] += cur;
+        i++;
+      } else if (cur === '"') quote = false;
+      else line[line.length - 1] += cur;
+    }
+  }
+  return ret;
+}
