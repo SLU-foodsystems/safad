2684c0820c1ae7eae7e15a9dc87059d716e67ccf
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-11-30 18:38:34 +0100

    Add some validation to parsers

--- src/lib/input-files-parsers.ts
+++ src/lib/input-files-parsers.ts
@@ -1,26 +1,70 @@
 import { ENV_IMPACTS_ZERO } from "@/lib/constants";
 import { parseCsvFile, roundToPrecision } from "@/lib/utils";
 
+const isNumerical = (str: string) => {
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i);
+    if (charCode < 48 || charCode > 57) return false;
+  }
+  return true;
+};
+
+const normalizeString = (str: string) => (str || "").trim();
+
 const asNumber = (str: string, elseValue = 0): number => {
-  const maybeNumber = Number.parseFloat((str || "").trim());
+  const maybeNumber = Number.parseFloat(normalizeString(str));
   return Number.isNaN(maybeNumber) ? elseValue : maybeNumber;
 };
 
+const asNumbers = (strs: string[], elseValue: number = 0): number[] =>
+  strs.map((x) => asNumber(x, elseValue));
+
+function ensureLength<T>(values: T[], length: number, elseValue: T) {
+  if (values.length === length) return values;
+
+  return Array.from({ length }).map((_none, i) => values[i] || elseValue);
+}
+
 export function parseEmissionsFactorsPackaging(csvString: string) {
-  const array = parseCsvFile(csvString)
+  const data = parseCsvFile(csvString)
     // Remove any empty rows
     .filter((row) => row.some((cell) => cell.length > 0))
     .slice(1, 10);
 
-  // TODO: Check if missing rows
-  // TODO: Check if invalid packaging types
-
-  return Object.fromEntries(
-    array.map(([pType, ...efs]) => [
-      pType,
-      efs.map((x) => Number.parseFloat(x)),
-    ])
+  const result = Object.fromEntries(
+    data
+      // Convert the strings to numbers
+      .map(([pType, ...efs]) => [pType, efs.map((x): number => asNumber(x))])
+      // Ensure exactly 3 emissions factors
+      .map(([pType, efs]) => [pType, ensureLength(efs as number[], 3, 0)])
   );
+
+  const expectedPackagingTypes = [
+    "P1",
+    "P2",
+    "P3",
+    "P4",
+    "P5",
+    "P6",
+    "P7",
+    "P8",
+    "P9",
+  ];
+
+  const missingPackagingTypes = expectedPackagingTypes.filter(
+    (pType) => !(pType in result)
+  );
+  if (missingPackagingTypes.length > 0) {
+    const prefix = "Error validating file, Emissions Factors for Packaging. ";
+    throw new Error(
+      prefix +
+        `Packaging types were missing in 1st col: ${missingPackagingTypes.join(
+          ", "
+        )}`
+    );
+  }
+
+  return result;
 }
 
 export function parseEmissionsFactorsEnergy(csvString: string) {
@@ -30,16 +74,41 @@ export function parseEmissionsFactorsEnergy(csvString: string) {
     { Electricity: {} };
 
   csv
-    .map((row) => row.map((x) => x.trim()))
+    .map((row) => row.map((x) => normalizeString(x)))
     .forEach(([carrier, _country, countryCode, ...ghgsStrs]) => {
-      const ghgs = ghgsStrs.map((x) => (x ? parseFloat(x) : 0));
+      const ghgs = asNumbers(ghgsStrs);
       if (carrier === "Electricity") {
         (emissionsFactors[carrier] as Record<string, number[]>)[countryCode] =
-          ghgs;
+          ensureLength(ghgs, 3, 0);
       } else {
-        emissionsFactors[carrier] = ghgs;
+        emissionsFactors[carrier] = ensureLength(ghgs, 3, 0);
       }
     });
+
+  const expectedCarrierTypes = [
+    "Electricity",
+    "Heating oil",
+    "Natural gas",
+    "Other fossil energy sources",
+    "Bark and chips",
+    "Pellets and briquettes",
+    "Other renewable energy sources",
+    "Diesel fuel",
+    "District heating",
+  ];
+
+  const missingCarrierTypes = expectedCarrierTypes.filter(
+    (carrier) => !(carrier in emissionsFactors)
+  );
+  if (missingCarrierTypes.length > 0) {
+    const prefix = "Error validating file, Emissions Factors for Energy. ";
+    throw new Error(
+      prefix +
+        `Packaging types were missing in 1st col: ${missingCarrierTypes.join(
+          ", "
+        )}`
+    );
+  }
 
   return emissionsFactors;
 }
@@ -50,7 +119,7 @@ export function parseEmissionsFactorsTransport(csvString: string) {
   const results: NestedRecord<string, number[]> = {};
 
   csv
-    .map((row) => row.map((x) => x.trim())) // Trim all fields
+    .map((row) => row.map((x) => normalizeString(x)))
     .forEach(
       ([
         consumptionCountryCode,
@@ -59,10 +128,7 @@ export function parseEmissionsFactorsTransport(csvString: string) {
         _productionCountry,
         ...ghgsStrs
       ]) => {
-        // Convert all GHG factors to numbers, falling back to 0 if missing
-        const ghgs = ghgsStrs
-          .map((x) => (x ? parseFloat(x) : 0))
-          .map((x) => (Number.isNaN(x) ? 0 : x));
+        const ghgs = ensureLength(asNumbers(ghgsStrs), 4, 0);
 
         if (!(consumptionCountryCode in results)) {
           results[consumptionCountryCode] = {};
@@ -78,15 +144,11 @@ export function parseEmissionsFactorsTransport(csvString: string) {
 export function parseProcessesEnergyDemands(csvString: string) {
   const csv = parseCsvFile(csvString).slice(1);
 
-  // TODO: ensure length of demandsStrs is correct
   return Object.fromEntries(
     csv.map(([code, _processName, _totalEnergy, _note, ...demandsStrs]) => {
-      // Covnert from strings to numbers
-      const demands = demandsStrs.map((x) => {
-        const val = Number.parseFloat(x) || 0;
-        return Number.isNaN(x) ? 0 : val;
-      });
-      return [code, demands];
+      // Convert from strings to numbers
+      const demands = asNumbers(demandsStrs);
+      return [normalizeString(code), ensureLength(demands, 11, 0)];
     })
   );
 }
@@ -94,9 +156,22 @@ export function parseProcessesEnergyDemands(csvString: string) {
 export function parseProcessesPackaging(csvString: string) {
   const data = parseCsvFile(csvString).slice(1);
 
-  const packagingData = Object.fromEntries(data.map((row) => [row[0], row[7]]));
+  const packagingData = Object.fromEntries(
+    data
+      .map((row) => [normalizeString(row[0]), normalizeString(row[7])])
+      // Skip all packaging values that are not "P1", "P2", ..., "P9"
+      .filter(
+        (pair) =>
+          pair[1].length === 2 &&
+          pair[1].charAt(0) === "P" &&
+          isNumerical(pair[1].charAt(1))
+      )
+  );
   const preparationProcessesData = Object.fromEntries(
-    data.map((row) => [row[2], row[4]]).filter((pair) => pair[1] !== "")
+    data
+      .map((row) => [normalizeString(row[2]), normalizeString(row[4])])
+      // Most rows will be empty, and that's fine. Ignore them.
+      .filter((pair) => pair[1] !== "")
   );
 
   return { ...packagingData, ...preparationProcessesData };
@@ -110,41 +185,36 @@ export function parseFootprintsRpcs(csvString: string) {
 
   data
     .filter((x) => x.length > 1)
-    .forEach(
-      ([
-        _i,
-        code,
-        _name,
-        _category,
-        _originName,
-        originCode,
-        ...impactsStr
-      ]) => {
-        if (code.trim() === "NA") return;
+    .map(
+      ([_i, code, _name, _category, _originName, originCode, ...impacts]) => [
+        normalizeString(code),
+        normalizeString(originCode),
+        ...impacts,
+      ]
+    )
+    .forEach(([code, originCode, ...impactsStr]) => {
+      if (code === "NA") return;
 
-        // Handle the base-case, i.e. the initial acc.
-        if (!(code in structured)) {
-          structured[code] = {};
-        }
-
-        if (originCode.toLowerCase().trim() === "row") {
-          originCode = "RoW";
-        }
-
-        structured[code][originCode] = impactsStr.map((x) => {
-          const val = parseFloat(x);
-          return Number.isNaN(val) ? 0 : val;
-        });
-
-        while (structured[code][originCode].length < EXPECTED_LENGTH) {
-          structured[code][originCode].push(0);
-        }
+      // Handle the base-case, i.e. the initial acc.
+      if (!(code in structured)) {
+        structured[code] = {};
       }
-    );
 
-  // Set RoW to be the average
+      // Avoid any inconsistent casing causing trouble.
+      if (originCode.toLowerCase() === "row") {
+        originCode = "RoW";
+      }
+
+      structured[code][originCode] = ensureLength(
+        asNumbers(impactsStr),
+        EXPECTED_LENGTH,
+        0
+      );
+    });
+
+  // Assing the average value as the "RoW"
   Object.entries(structured).forEach(([suaCode, factorsPerOrigin]) => {
-    // Abort if RoW already is defined
+    // Abort if RoW already is defined, i.e. avoid overwriting it
     if (Object.keys(factorsPerOrigin).includes("RoW")) return;
 
     const numberOfOrigins = Object.values(factorsPerOrigin).length;
@@ -168,7 +238,7 @@ export function parseWasteRetailAndConsumer(csvString: string) {
 
   return Object.fromEntries(
     data.map(([code, _name, retailWaste, consumerWaste]) => [
-      code,
+      normalizeString(code),
       [asNumber(retailWaste), asNumber(consumerWaste)],
     ])
   );
@@ -177,10 +247,9 @@ export function parseWasteRetailAndConsumer(csvString: string) {
 export function parseDiet(csvString: string): Diet {
   const data = parseCsvFile(csvString).slice(1);
 
-  return data.map(([code, _name, amount]): FoodEntry => [
-    code,
-    asNumber(amount),
-  ]);
+  return data.map(
+    ([code, _name, amount]): FoodEntry => [normalizeString(code), asNumber(amount)]
+  );
 }
 
 export function parseRpcOriginWaste(csvString: string) {
@@ -255,14 +324,14 @@ export function parseFoodsRecipes(recipesCsvStr: string) {
       ]) => {
         if (code === "") return; // Empty row
         const value = roundToPrecision(
-          (parseFloat(perc) * parseFloat(prob)) / 100,
+          (asNumber(perc, 1) * asNumber(prob, 1)) / 100,
           3
         );
 
-        const netYieldFactor =
-          (parseFloat(yieldFactor) || 1) * (parseFloat(allocationFactor) || 1);
-
         if (value === 0) return;
+
+        const netYieldFactor =
+          asNumber(yieldFactor, 1) * asNumber(allocationFactor, 1);
 
         const PROCESS_UNSPECIFIED = "F28.A07XD";
         const processes = facetStr
@@ -273,7 +342,7 @@ export function parseFoodsRecipes(recipesCsvStr: string) {
           component,
           processes,
           value,
-          netYieldFactor || 1,
+          netYieldFactor,
         ];
 
         if (code in recipes) {
