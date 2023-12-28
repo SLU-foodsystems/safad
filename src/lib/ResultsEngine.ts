a359bb4aa3f1a29e799384e4ac634665971be43c
Wilhelm Wanecek <wilhelm@wanecek.com>
2023-12-28 12:38:11 +0100

    Allow level 2 codes in foods impacts

--- src/lib/ResultsEngine.ts
+++ src/lib/ResultsEngine.ts
@@ -314,7 +314,7 @@ class ResultsEngine {
 
     const fauxDiet: Diet = [...codesInRecipes]
       .sort()
-      .filter(code => getRpcCodeLevel(code) > 2)
+      .filter(code => getRpcCodeLevel(code) > 1)
       .map((code) => [code, 1000]);
     return this.computeImpactsDetailed(fauxDiet);
   }
