2d7c0b1f64bbcb376c41e43c95dc8dc40db9bfe1
Wilhelm Wanecek <wilhelm@wanecek.com>
2024-02-29 16:14:09 +0100

    Bugfix: update N_TRANSPORT_GHGS

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,7 +8,7 @@ export const PROCESS_EMISSIONS_ZERO = zeroes(N_PROCESS_GHGS);
 
 export const N_PACKAGING_GHGS = 4;
 
-export const N_TRANSPORT_GHGS = 4;
+export const N_TRANSPORT_GHGS = 3;
 export const TRANSPORT_EMISSIONS_ZERO = zeroes(N_TRANSPORT_GHGS);
 
 // The planetary boundaries used as limits.
