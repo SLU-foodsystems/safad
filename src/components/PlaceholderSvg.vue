53b82ff1af69cbb069275974ef421d0309614e0a
Wilhelm Wanecek <wilhelm@wanecek.com>
2024-03-02 12:38:02 +0100

    Formatting

--- src/components/PlaceholderSvg.vue
+++ src/components/PlaceholderSvg.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 const props = defineProps<{
   aspectRatio: number;
 }>();
@@ -7,24 +6,29 @@ const props = defineProps<{
 const width = 100;
 const height = width * props.aspectRatio;
 
-const viewBox = `0 0 ${width} ${height}`
+const viewBox = `0 0 ${width} ${height}`;
 </script>
 
 <template>
-  <svg :width="width" :height="height" :viewBox="viewBox" fill="none" xmlns="http://www.w3.org/2000/svg">
-  </svg>
+  <svg
+    :width="width"
+    :height="height"
+    :viewBox="viewBox"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  ></svg>
 </template>
 
 <style lang="scss" scoped>
-  svg {
-    width: 100%;
-    height: auto;
-    position: relative;
+svg {
+  width: 100%;
+  height: auto;
+  position: relative;
 
-    background-image: url("@/assets/spinner.svg");
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 3em 3em;
-    animation: rotate 1s linear infinite;
-  }
+  background-image: url("@/assets/spinner.svg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 3em 3em;
+  animation: rotate 1s linear infinite;
+}
 </style>
