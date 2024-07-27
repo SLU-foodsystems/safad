1b8fe9654aef86204579d517f6545191f32ccddd
Wilhelm Wanecek <wilhelm@wanecek.com>
2024-07-27 12:15:22 +0200

    Update the kastner script
    * Remove old ad-hoc splicing functionality that corrected old bugs
    * Remove dependency on existing files

--- data-preprocessing/rpc-parameters/generate-csv-per-country.mjs
+++ data-preprocessing/rpc-parameters/generate-csv-per-country.mjs
@@ -1,11 +1,10 @@
-#!/usr/bin/env node
 //@ts-check
 
 /**
  * Script for creating a sheets with RPC parameters for different countries.
  *
- * Instead of taking the list of files as arguments, as most other scripts in
- * this project does, his defines all paths in the function main() below.
+ * NOTE: Instead of taking the list of files as arguments, as most other scripts
+ * in this project does, all paths are defined in the function main() below.
  *
  * The only input it takes is the list of countries to use generate csvs for.
  */
@@ -13,9 +12,10 @@
 import * as path from "path";
 import url from "url";
 
-import { readCsv, roundToPrecision, sum, uniq } from "../utils.mjs";
+import { readCsv, roundToPrecision, uniq } from "../utils.mjs";
 
-import countryCodes from "./country-codes.json" assert { type: "json" };
+import countryCodes from "./country-codes.json" with { type: "json" };
+import countryNames from "./country-names.json" with { type: "json" };
 
 const RESULT_PRECISION = 3;
 const DEBUG_PRINT_ITEMNAMES = false;
@@ -36,9 +36,33 @@ const COUNTRY_RENAME_MAP = {
 };
 
 const ALL_COUNTRY_OVERRIDES = [
-  ["01802", "Sugar cane", "Sugar crops", "ES", 1, 0.045, 0],
-  ["01491.01", "Oil palm fruit", "Oil crops, tree", "MY", 0.3, 0.06, 0],
-  ["01491.01", "Oil palm fruit", "Oil crops, tree", "ID", 0.7, 0.06, 0],
+  [
+    "A.02.08.002",
+    "Sugar cane (Saccharum officinarum)",
+    "Spain",
+    "ES",
+    1,
+    0.045,
+    "01802",
+  ],
+  [
+    "A.05.05.003",
+    "Palmfruit (Elaeis guineensis)",
+    "Malaysia",
+    "MY",
+    0.3,
+    0.06,
+    "01491.01",
+  ],
+  [
+    "A.05.05.003",
+    "Palmfruit (Elaeis guineensis)",
+    "Indonesia",
+    "ID",
+    0.7,
+    0.06,
+    "01491.01",
+  ],
 ];
 
 // Sua codes in the list above
@@ -53,7 +77,6 @@ const maybeQuote = (value) =>
     ? `"${value}"`
     : value;
 
-const _countries = new Set();
 /**
  * Most complex logic in this file, where we extract the shares from the trade
  * matrix for a specific country.
@@ -62,13 +85,13 @@ const _countries = new Set();
  * portion (%) is produced in each producerCountry.
  *
  * @param {string[][]} matrix
- * @param {string} country
+ * @param {string} consumerCountry
  * @returns {Object.<string, Object.<string, number>>}
  */
-function getFoodItemShares(matrix, country) {
+function getFoodItemShares(matrix, consumerCountry) {
   const filtered = matrix
     // Skip all other countries
-    .filter((row) => row[6] == country)
+    .filter((row) => row[6] == consumerCountry)
     .map((x) => ({
       amount: parseFloat(x[3]),
       producerCountry: x[7],
@@ -88,7 +111,7 @@ function getFoodItemShares(matrix, country) {
     );
 
   if (filtered.length === 0) {
-    throw new Error("Country " + country + " not found.");
+    throw new Error("Country " + consumerCountry + " not found.");
   }
 
   /**
@@ -103,6 +126,7 @@ function getFoodItemShares(matrix, country) {
    * STEP 2: Compute the proportions:
    * { [itemName]: { [producerCountry]: percentage }}
    */
+  /** @type {Object.<string, Object.<string, number>>} */
   const allProportions = {};
   filtered.forEach(({ amount, producerCountry, itemName }) => {
     if (!(itemName in allProportions)) {
@@ -115,43 +139,13 @@ function getFoodItemShares(matrix, country) {
       );
     }
 
-    allProportions[itemName][producerCountry] = amount / totalAmounts[itemName];
-  });
-
-  /**
-   * STEP 3: Simplify the above proportions by aggregating all values below a
-   * certain threshold to a 'rest of world' key.
-   */
-  /** @type {Object.<string, Object.<string, number>>} */
-  const simplifiedProportions = {};
-  Object.keys(allProportions).forEach((itemName) => {
-    const result = {};
-
-    // Step 3a: Trasnfer all values
-    Object.entries(allProportions[itemName]).forEach(([country, value]) => {
-      result[country] = value;
-      _countries.add(country);
-    });
-
-    // Steb 3b: Add whatever is left of 100% as RoW
-    const importsSum = sum(Object.values(result));
-    result.RoW = Math.max(0, 1 - importsSum);
-
-    // Step 3c: Round to precision to avoid too many decimal points
-    simplifiedProportions[itemName] = Object.fromEntries(
-      Object.entries(result)
-        .map(
-          ([k, v]) =>
-            /** @type {[string, number]} */ ([
-              k,
-              roundToPrecision(v, RESULT_PRECISION),
-            ])
-        )
-        .filter(([_k, v]) => v > 0)
+    allProportions[itemName][producerCountry] = roundToPrecision(
+      amount / totalAmounts[itemName],
+      RESULT_PRECISION
     );
   });
 
-  return simplifiedProportions;
+  return allProportions;
 }
 
 /**
@@ -174,16 +168,41 @@ function createCountryWastesMap(rows) {
 }
 
 /**
+ * @param {string[][]} rpcToSuaCodesCsv
+ */
+function createRpcSuaTranslationMaps(rpcToSuaCodesCsv) {
+  const rpcToSua = {};
+  const suaToRpcs = {};
+  const rpcNames = {};
+  rpcToSuaCodesCsv.forEach(([_foodEx2Code, rpcCode, rpcName, suaCode]) => {
+    // Add rpcToSua codes: many-to-one
+    rpcToSua[rpcCode] = suaCode;
+    rpcNames[rpcCode] = rpcName;
+
+    // Add suaToRpcs codes: one-to-many
+    if (!suaToRpcs[suaCode]) {
+      suaToRpcs[suaCode] = [];
+    }
+    suaToRpcs[suaCode].push(rpcCode);
+  });
+
+  return { rpcToSua, suaToRpcs, rpcNames };
+}
+
+/**
  * Main function
  *
  * @param {string[]} args
  */
 function main(args) {
-  const [consumerCountry, oldFilePath] = args;
+  if (args.length === 0 || !args[0]) {
+    throw new Error(
+      "Script expects one argument: <countryname>:\n\n" +
+        "\tnode generate-csv-per-country.mjs Sweden\n"
+    );
+  }
 
-  const rpcOriginWasteRows = readCsv(path.resolve(DIRNAME, oldFilePath)).slice(
-    1
-  );
+  const [consumerCountry] = args;
 
   // Input file: csv of column with category (as defined in rpc) and waste
   // Create an object with { [country: string]: { [category: string]: number } }
@@ -219,24 +238,15 @@ function main(args) {
   /** @type Array<string | number>[] */
   const fullKastnerDataRows = [];
 
-  const rpcToSua = {};
-  const suaToRpcs = {};
-  rpcToSuaCodesCsv.forEach(([_foodEx2Code, rpcCode, _rpcName, suaCode]) => {
-    // Add rpcToSua codes: many-to-one
-    rpcToSua[rpcCode] = suaCode;
-
-    // Add suaToRpcs codes: one-to-many
-    if (!suaToRpcs[suaCode]) {
-      suaToRpcs[suaCode] = [];
-    }
-    suaToRpcs[suaCode].push(rpcCode);
-  });
+  const { suaToRpcs, rpcNames } = createRpcSuaTranslationMaps(rpcToSuaCodesCsv);
 
   /**
    * The part of script where we put all parts together.
    *
    * We iterate over the "sua template", where each row is a food item (sua-code
-   * , name, and category) to construct the final results.
+   * , name, and category) to construct the output file.
+   *
+   * For each sua-code (in this list), we output the N origin rows.
    */
   suaKastnerList.forEach((row, i) => {
     const [suaCode, suaName, _category, isPerfectMatch, altItemName] = row;
@@ -265,145 +275,40 @@ function main(args) {
       waste = wasteFactorsMap[consumerCountry][category];
     }
 
-    // And we store in the final results as a list, to be made into a csv.
-    Object.entries(shares).forEach(([prodCountry, share]) => {
-      fullKastnerDataRows.push([
-        suaCode,
-        suaName,
-        category,
-        prodCountry,
-        share,
-        waste,
-        0, // organic
-      ]);
+    /** @type {string[]} a*/
+    const rpcCodes = suaToRpcs[suaCode];
+    if (!rpcCodes) {
+      // throw new Error("No rpc-codes found for sua " + suaCode);
+      return;
+    }
+
+    rpcCodes.forEach((rpcCode) => {
+      //"RPC Code,RPC Name,Producer Country Name,Producer Country Code,Share,Waste,SUA Code";
+      // And we store in the final results as a list, to be made into a csv.
+      Object.entries(shares).forEach(([prodCountry, share]) => {
+        if (share < 0.01) return;
+        fullKastnerDataRows.push([
+          rpcCode,
+          rpcNames[rpcCode],
+          countryNames[prodCountry],
+          prodCountry,
+          share,
+          waste,
+          suaCode,
+        ]);
+      });
     });
   });
 
+  // Add the overrides that we hard-code to all files.
   ALL_COUNTRY_OVERRIDES.forEach((row) => {
     fullKastnerDataRows.push(row);
-  });
-
-  const FLOPPED_COUNTRIES_NEW_VALUES = ["PT", "GF", "JM"];
-  const FLOPPED_COUNTRIES_PREV_VALUES = ["PL", "GH", "MM"];
-  const FLOPPED_COUNTRIES_OLD_TO_NEW = {
-    PL: "PT",
-    GH: "GF",
-    MM: "JM",
-  };
-  const FLOPPED_COUNTRIES_NEW_TO_OLD = {
-    PT: "PL",
-    GF: "GH",
-    JM: "MM",
-  };
-  const FLOPPED_COUNTRIES_OLD_TO_NEW_NAMES = {
-    Poland: "Portugal",
-    Ghana: "French Guiana",
-    Myanmar: "Jamaica",
-  };
-
-  const correctedEntries = fullKastnerDataRows.filter(
-    ([_suaCode, _suaName, _category, country]) =>
-      FLOPPED_COUNTRIES_NEW_VALUES.includes(/** @type {string}*/ (country))
-  );
-
-  const countriesWhereSuaFloppedIn = {};
-  correctedEntries.forEach(([suaCode, _suaName, _cat, country]) => {
-    if (!countriesWhereSuaFloppedIn[suaCode]) {
-      countriesWhereSuaFloppedIn[suaCode] = [];
-    }
-
-    countriesWhereSuaFloppedIn[suaCode].push(
-      FLOPPED_COUNTRIES_NEW_TO_OLD[country]
-    );
-  });
-
-  const floppedSuas = new Set(correctedEntries.map((x) => x[0]));
-
-  // Check for duplicates
-  const hits = new Set();
-  rpcOriginWasteRows.forEach(
-    ([
-      rpcCode,
-      _rpcName,
-      _countryName,
-      countryCode,
-      _share,
-      _waste,
-      suaCode,
-    ]) => {
-      if (!floppedSuas.has(suaCode)) return;
-      if (!floppedSuas.has(suaCode)) return;
-      const key = rpcCode + "|" + suaCode + "|" + countryCode;
-      if (!hits.has(key)) hits.add(key);
-      else {
-        throw new Error("NOO!\t\t" + consumerCountry + " : " + key);
-      }
-    }
-  );
-
-  const splicedData = rpcOriginWasteRows.map((row) => {
-    const [rpcCode, rpcName, countryName, countryCode, share, waste, suaCode] =
-      row;
-    // First, we check if it's a relevant SUA-code, i.e. a block where there's
-    // an issue at all
-    if (!floppedSuas.has(suaCode)) return row;
-    // Second, we check if this specific country is involved, e.g. if it's
-    // Poland
-    if (!FLOPPED_COUNTRIES_PREV_VALUES.includes(countryCode)) return row;
-
-    // Third, now make sure that it's a relevant match for this specific
-    // sua-country pair, e.g. (01212,Poland), not (01212, Ghana)
-    if (!countriesWhereSuaFloppedIn[suaCode].includes(countryCode)) {
-      return row;
-    }
-
-    const correctedEntryMatches = correctedEntries.filter(
-      ([
-        expectedSuaCode,
-        _suaName,
-        _category,
-        prodCountryCode,
-        expectedShare,
-        expectedWaste,
-      ]) =>
-        // Check that country and sua match
-        expectedSuaCode === suaCode &&
-        FLOPPED_COUNTRIES_NEW_TO_OLD[prodCountryCode] === countryCode &&
-        // and that it's the 'wrong' one we correct, not the actual value
-        // (i.e. that it says Poland but should be Portugal)
-        waste === String(expectedWaste) &&
-        share === String(expectedShare)
-    );
-
-    if (correctedEntryMatches.length !== 1) {
-      console.log("");
-      console.log(row);
-      console.log("");
-      console.log(correctedEntryMatches);
-      console.log("");
-      console.log(correctedEntries.join("\n"));
-      throw new Error("err");
-    }
-
-    const correctedName = FLOPPED_COUNTRIES_OLD_TO_NEW_NAMES[countryName];
-    if (!correctedName) {
-      throw new Error("No name found for " + countryName);
-    }
-    return [
-      rpcCode,
-      rpcName,
-      correctedName,
-      FLOPPED_COUNTRIES_OLD_TO_NEW[countryCode],
-      share,
-      waste,
-      suaCode,
-    ];
   });
 
   const HEADER =
     "RPC Code,RPC Name,Producer Country Name,Producer Country Code,Share,Waste,SUA Code";
 
-  const body = splicedData
+  const body = fullKastnerDataRows
     .map((x) => x.map((val) => maybeQuote(val)).join(","))
     .join("\n");
 
