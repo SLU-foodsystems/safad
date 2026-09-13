8e6e9e428f597a939c96a0d903c911d3e7919c43
Wilhelm Wanecek <wilhelm@wanecek.com>
2026-09-13 12:08:41 +0200

    Update vitest and vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import UnpluginInjectPreload from "unplugin-inject-preload/vite";
 
-import statCsvFiles from "./build-utils/stat-input-files";
+import statCsvFiles from "./build-utils/stat-input-files.ts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
